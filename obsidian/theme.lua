d10286e95c782b4370f8f3393107c6588d4f1fb4
guiddo806 <korovkotymur@gmail.com>
2025-04-24 15:37:38 +0300

    theme.lua

--- obsidian/theme.lua
+++ obsidian/theme.lua
@@ -37,7 +37,7 @@ local ThemeManager = {} do
 
     ThemeManager.Library = nil
     ThemeManager.BuiltInThemes = {
-        ["Default"] 		    = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"00000","AccentColor":"6c607d","BackgroundColor":"00000","OutlineColor":"1f1d22"}') },
+        ["Default"] 		    = { 1, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"ooooo","AccentColor":"6c607d","BackgroundColor":"ooooo","OutlineColor":"1f1d22"}') },
         ["BBot"] 			    = { 2, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"1e1e1e","AccentColor":"7e48a3","BackgroundColor":"232323","OutlineColor":"141414"}]]) },
         ["Fatality"]		    = { 3, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"1e1842","AccentColor":"c50754","BackgroundColor":"191335","OutlineColor":"3c355d"}]]) },
         ["Jester"] 		    	= { 4, httpService:JSONDecode([[{"FontColor":"ffffff","MainColor":"242424","AccentColor":"db4467","BackgroundColor":"1c1c1c","OutlineColor":"373737"}]]) },
